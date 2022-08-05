211015be2b2cce167158582f66bb9bdb335a9a69
shai9 <dennis.shakava@student.moringaschool.com>
2022-08-05 21:43:06 +0300

    about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ let Home = () => {
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
         <p className='text-white text-2xl'> Hello there, my name is, </p>
         <h1 className='text-6xl sm:text-7xl font-extrabold text-[#092bc2]'>Dennis Shakava</h1>
-        <h2 className='text-4xl sm:text-4xl font-semicold text-white '>I'm a Full-Stack Developer.</h2>
+        <h2 className='sm:text-4xl font-semibold text-white'>I'm a Full-Stack Developer.</h2>
         <p className='text-2xl sm:text-2xl font-light text-white '>Im a passionate Kenyan Software Developer. I'm a weird guy who likes   making weird things   with Web Languages.Currently, I'm focussed on building responsive full-stack web applications.When not working or futzing around with code, I study how we as humans function.</p>
     <div>
       <button className='text-white border-2 px-9 py-3 my-2 flex items-center hover:bg-[#0b31d8] hover:border-blue-600'>Work</button>
