9d7be35bf3fae73c25d90ff667e7767a3f0d2872
shai9 <dennis.shakava@student.moringaschool.com>
2022-08-05 18:08:21 +0300

    changes on home

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let About = ()=> {
+  return (
+    <div name = 'about' className='w-full' h-screen >
+
+    </div>
+  )
+}
+
+export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 let Home = () => {
   return (
-    <div className='w-full h-screen bg-black'>
+    <div name ='home' className='w-full h-screen bg-black'>
       {/* container */}
-      <div className='max-v-[100px] mx-auto px-8 flex flex-col justify-center h-full '>
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
         <p className='text-white text-2xl'> Hello there, my name is, </p>
         <h1 className='text-6xl sm:text-7xl font-extrabold text-[#092bc2]'>Dennis Shakava</h1>
-        <h2 className='text-4xl sm:text-4xl font-semicold text-white '>I am a Full-Stack Developer.</h2>
-        <p className='text-2xl sm:text-2xl font-light text-white '>Iam a passionate Kenyan Software Developer. I'm a weird guy who likes   making weird things   with Web Languages.Currently, I'm focussed on building responsive full-stack web applications.When not working or futzing around with code, I study how we as humans function.</p>
+        <h2 className='text-4xl sm:text-4xl font-semicold text-white '>I'm a Full-Stack Developer.</h2>
+        <p className='text-2xl sm:text-2xl font-light text-white '>Im a passionate Kenyan Software Developer. I'm a weird guy who likes   making weird things   with Web Languages.Currently, I'm focussed on building responsive full-stack web applications.When not working or futzing around with code, I study how we as humans function.</p>
     <div>
-      <button className='text-white border-2 px-6 py-1  items-center hover:bg-[#0b31d8]'>Work</button>
+      <button className='text-white border-2 px-9 py-3 my-2 flex items-center hover:bg-[#0b31d8] hover:border-blue-600'>Work</button>
     </div>
       </div>
     </div>
