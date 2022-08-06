5be64d9d1777df84e88c3768bbbf204f11b5342c
shai9 <dennis.shakava@student.moringaschool.com>
2022-08-06 20:40:41 +0300

    works

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
+import Work from './components/Work';
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
    <Home />
    <About/>
    <Skills/>
-   {/* <Work />
-   <Contact /> */}
+   <Work/>
+   {/* <Contact /> */}
    
    </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,8 +45,8 @@ const Navbar = () => {
           <li className='py-6 text-4xl'>Contact</li>
        </ul> 
         {/*socials  */}
-        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-        <ul>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0 '>
+        <ul className='font-light'>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
